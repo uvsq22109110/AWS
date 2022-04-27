4217700115ce4f128d3ace751518daec9e380494
uvsq21805607 <72140554+uvsq21805607@users.noreply.github.com>
2022-04-27 19:55:35 +0200

    Reservation implementée

--- API/routes/reservations.js
+++ API/routes/reservations.js
@@ -2,20 +2,30 @@ const express = require('express');
 const router = express.Router();
 const ResaID = require('mongoose').Types.ObjectId; // recupere l id de la reservation 
 const {Reservation} = require('../models/Reservation') ;
+const resto = require('../models/Resto');
 
 //Affciher les reservations 
-router.get('/', (req, res) => {
+// router.get('/', (req, res) => {
 
-	Reservation.find((err, resa) => {
-		if(!err) res.send(resa);
-		else
-			console.log("Erreur de recuperation de data") + err;
-	})
+// 	Reservation.find((err, resa) => {
+// 		if(!err) 
+// 			Reservation.find()
+//       		.populate("restaurant_id", ).exec((err, details_restaurants) => {
+//       		res.send(details_restaurants);
+//    	 })
+       
+// 		else
+// 			console.log("Erreur de recuperation de data") + err;
+// 	})
 
-});
+// 	res.render('reservation'); 
+		
+// });
 
 //Ajouter Une reservation
-router.post('/', (req, res) => {
+router.post('/',  (req, res) => {
+
+	console.log(req.body);
 
 	const newRecord = new Reservation({
 
@@ -29,51 +39,65 @@ router.post('/', (req, res) => {
 	
 		Nombre_de_personnes : req.body.Nombre_de_personnes,
 
-		date: req.body.date
+		date: req.body.date,
+
+		// restaurant_id: req.params.id
 	});
 
-	newRecord.save((err, resa) => {
-		if (!err) res.send(resa);
+	   newRecord.save((err, resa) => {
+		if (!err) 
+			{const result = Reservation.find()
+			  .populate("restaurant_id", ).exec((err, details_restaurants) => {
+			    res.send(details_restaurants);
+			   	})
+			      		res.render('fin_de_reservations',{resa: result});}
+				
 		else
-		  console.log("Erreur de ceration de new data : " + err);
+		  console.log("Erreur de creation de new data : " + err);
 
 	})
+
+	
+
 });
+
 
 //Modifier une reservation 
 
-router.put("/:id", (req, res) =>  {
+// router.put("/:id/:id1", (req, res) =>  {
 
-	if (!ResaID.isValid(req.params.id))
-		return res.status(400).send("ID INCONNU : " + req.params.id);
+// 	if (!ResaID.isValid(req.params.id))
+// 		return res.status(400).send("ID INCONNU : " + req.params.id);
 	
 	
-	const modifResa = {
+// 	const modifResa = {
 
-		Nom_client: req.body.Nom_client,
+// 		Nom_client: req.body.Nom_client,
 
-		Prenom_client: req.body.Prenom_client,
+// 		Prenom_client: req.body.Prenom_client,
 
-		Num_tel_client: req.body.Num_tel_client,
+// 		Num_tel_client: req.body.Num_tel_client,
 
-		Mail_client: req.body.Mail_client,
+// 		Mail_client: req.body.Mail_client,
 	
-		Nombre_de_personnes : req.body.Nombre_de_personnes,
+// 		Nombre_de_personnes : req.body.Nombre_de_personnes,
 
-		date: req.body.date
-	};
+// 		date: req.body.date,
 
-	Reservation.findByIdAndUpdate(
-		req.params.id,
-		{ $set: modifResa },
-		{ new: true},
-		(err, resa) => {
-			if(!err) res.send(resa);
-			else
-				console.log("Erreur lors de la modification : " + err);
-	})
+// 		restaurant_id: req.params.id1
+// 	};
+
+// 	Reservation.findByIdAndUpdate(
+// 		req.params.id,
+// 		{ $set: modifResa },
+// 		{ new: true},
+// 		(err, resa) => {
+// 			if(!err) res.send(resa);
+// 			else
+// 				console.log("Erreur lors de la modification : " + err);
+// 	})
 	
-});
+// });
 
 
 //Supprimer ( Annuler ) reservation 
