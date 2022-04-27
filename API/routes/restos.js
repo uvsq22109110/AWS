43deccb4aaf14e959929743646220448f0107a23
uvsq22109110 <khaled.mebhah@gmail.com>
2022-04-27 15:13:32 +0200

    Pagination v1

--- API/routes/restos.js
+++ API/routes/restos.js
@@ -6,22 +6,79 @@ const Resto = require('../models/Resto');
 
 const router = express.Router();
 
-router.post('/', async (req, res) => {
+var cuisine = "Tout";
+var ville = "Paris";
+
+// router.post('/', async (req, res) => {
     
-    try{
-        console.log(req.body);
-        if(req.body.cuisine == 'Tout'){
-            const restos = await Resto.find({ 'ville' : req.body.ville});
-            res.render("discover", {data : restos});
-        } else{
-            const restos = await Resto.find({ 'cuisine' : req.body.cuisine, 'ville' : req.body.ville});
-            res.render("discover", {data : restos});
-        }
+//     try{
+//         console.log(req.body);
+//         if(req.body.cuisine == 'Tout'){
+//             const restos = await Resto.find({ 'ville' : req.body.ville});
+//             res.render("discover", {data : restos});
+//         } else{
+//             const restos = await Resto.find({ 'cuisine' : req.body.cuisine, 'ville' : req.body.ville});
+//             res.render("discover", {data : restos});
+//         }
         
+//     }
+//     catch (err){
+//         res.json({ message : err });
+//     }
+// });
+function pagination(req, res, next) {
+    var perPage = 9
+    var page = req.params.page || 1
+    console.log(page);
+    console.log(cuisine);
+    console.log(ville);
+    if(req.body.cuisine == 'Tout'){
+        Resto
+        .find({'ville' : ville})
+        .skip((perPage * page) - perPage)
+        .limit(perPage)
+        .exec(function(err, restos) {
+            Resto.count().exec(function(err, count) {
+                if (err) return next(err)
+                res.render('discover', {
+                    data: restos,
+                    current: page,
+                    pages: Math.ceil(count / perPage)
+                })
+            })
+        });
     }
-    catch (err){
-        res.json({ message : err });
+    else{
+        Resto
+        .find({'cuisine':cuisine ,'ville' : ville})
+        .skip((perPage * page) - perPage)
+        .limit(perPage)
+        .exec(function(err, restos) {
+            Resto.count().exec(function(err, count) {
+                if (err) return next(err)
+                res.render('discover', {
+                    data: restos,
+                    current: page,
+                    pages: Math.ceil(count / perPage)
+                })
+            })
+        });
     }
+    
+}
+
+router.post('/', function(req, res, next) {
+    cuisine = req.body.cuisine;
+    ville = req.body.ville;
+    pagination(req, res, next)
+});
+
+// router.post('/:page', function(req, res, next) {
+//     pagination(req, res, next)
+// });
+
+router.get('/:page', function(req, res, next) {
+    pagination(req, res, next)
 });
 
 router.get('/', (req, res) => {
