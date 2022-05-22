d3cafa1cbe4a8de5813ebbdbb7b6ce2afd4e2230
uvsq21805607 <72140554+uvsq21805607@users.noreply.github.com>
2022-05-22 17:15:09 +0200

    contact.js post

--- API/routes/contact.js
+++ API/routes/contact.js
@@ -1,18 +1,52 @@
-const inputs = document.querySelectorAll(".input");
+const express = require('express');
+const router = express.Router();
+const cont = require('../models/Contact');
 
-function focusFunc() {
-  let parent = this.parentNode;
-  parent.classList.add("focus");
-}
 
-function blurFunc() {
-  let parent = this.parentNode;
-  if (this.value == "") {
-    parent.classList.remove("focus");
+//Ajouter Un message
+router.post('/',  (req, res) => {
+
+  console.log(req.body);
+
+  const newmessage = new cont({
+
+    Nom: req.body.Nom,
+
+    Prenom: req.body.Prenom,
+
+    Num_tel: req.body.Num_tel,
+
+    Mail: req.body.Mail,
+  
+    message: req.body.message    
   }
-}
 
-inputs.forEach((input) => {
-  input.addEventListener("focus", focusFunc);
-  input.addEventListener("blur", blurFunc);
+
+);
+
+    newmessage.save((err, resa) => {
+    if (!err) {
+
+      
+
+      res.render("Message_envoye", {nom : req.body.Nom, prenom : req.body.Prenom, 
+      Telephone : req.body.Num_tel, Mail : req.body.Mail , 
+      Message : req.body.message }
+      );
+
+
+    }
+    else
+      console.log(err);
+      res.status(401).send(
+                "Erreur de saisi : \n Vous avez oublié un champs ou mal le saisir \n Veuillez revenir en arriere pour corriger ! \n ");
+
+
+  })
+  
+
 });
+
+
+
+module.exports = router;
